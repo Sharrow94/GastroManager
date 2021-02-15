2029443a25adbd0b8a4492b6b2b57c82d0022b95
Zawada992 <r.zawadzki92@gmail.com>
2021-02-15 10:31:59 +0100

    All Entity

--- src/main/java/pl/gastromanager/gastromanager/model/Client.java
+++ src/main/java/pl/gastromanager/gastromanager/model/Client.java
@@ -1,7 +1,13 @@
 package pl.gastromanager.gastromanager.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }

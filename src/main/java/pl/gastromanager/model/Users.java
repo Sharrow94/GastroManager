51aeabaf1a3832e0f18468ce1775d23699bf20d4
michals3r3k <serek.michal0827@gmail.com>
2021-02-20 17:06:51 +0100

    Logowanie za pomocą Google

--- src/main/java/pl/gastromanager/model/Users.java
+++ src/main/java/pl/gastromanager/model/Users.java
@@ -8,7 +8,6 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-import java.util.List;
 import java.util.Set;
 
 @Data
@@ -18,27 +17,32 @@ public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank
+//    @NotBlank
     private String userName;
     @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[\\!\\@\\#\\$\\%\\^\\&\\*\\(\\)\\_\\+\\-\\=])(?=.*[A-Z])(?!.*\\s).{8,}$")
     private String password;
     @Email
     private String email;
     @NotBlank
-    @Pattern(regexp = "[A-Z][a-zA-Z]*")
+//    @Pattern(regexp = "[A-Z][a-zA-Z]*")
     private String firstName;
-    @NotBlank
-    @Pattern(regexp = "[A-Z][a-zA-Z]*")
+//    @NotBlank
+//    @Pattern(regexp = "[A-Z][a-zA-Z]*")
     private String lastName;
     private int enabled;
-    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "users_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles;
     private float salary;
+    //    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    //    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "users_id"),
+//            inverseJoinColumns = @JoinColumn(name = "role_id"))
+//    private Set<Role> roles;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role")
+    private Set<Role> roles;
     private String phoneNumber;
     private String city;
     private String street;
     private String nr;
     private String postNumber;
+    @Enumerated(EnumType.STRING)
+    private AuthProvider authProvider;
 }

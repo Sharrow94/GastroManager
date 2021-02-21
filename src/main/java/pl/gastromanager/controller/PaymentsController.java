51561581df7bd208c94f5a17b1eabc75e5a208dd
Franciszek Szaniecki <franekszaniecki@gmail.com>
2021-02-21 22:09:22 +0100

    SupplierAllView-correction

--- src/main/java/pl/gastromanager/controller/PaymentsController.java
+++ src/main/java/pl/gastromanager/controller/PaymentsController.java
@@ -1,12 +1,18 @@
 package pl.gastromanager.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.gastromanager.model.Payments;
+import pl.gastromanager.model.Users;
+import pl.gastromanager.repository.RoleRepository;
 import pl.gastromanager.service.PaymentsService;
+import pl.gastromanager.service.UserService;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -14,15 +20,29 @@ import java.util.Optional;
 @RequestMapping("/payments")
 public class PaymentsController {
     private final PaymentsService paymentsService;
+    private final UserService userService;
+    private final RoleRepository roleRepository;
 
-    public PaymentsController(PaymentsService paymentsService) {
+    public PaymentsController(PaymentsService paymentsService, UserService userService, RoleRepository roleRepository) {
         this.paymentsService = paymentsService;
+        this.userService = userService;
+        this.roleRepository = roleRepository;
     }
 
     @RequestMapping("/all")
     public String showAllPayments(Model model) {
-        List<Payments> payments = paymentsService.findAllPayments();
-        model.addAttribute("payments", payments);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Users currentUser = userService.findByUserName(auth.getName());
+        Long id = currentUser.getId();
+        List<String>roles=paymentsService.findAllRolesByUserId(id);
+
+        if (roles.contains("ROLE_ADMIN" )) {
+            List<Payments> paymentsAll = paymentsService.findAllPayments();
+            model.addAttribute("payments", paymentsAll);
+        } else {
+            List<Payments> payments = paymentsService.findPaymentsByUserId(id);
+            model.addAttribute("payments", payments);
+        }
         return "payments/list";
     }
 
@@ -32,7 +52,6 @@ public class PaymentsController {
         model.addAttribute("payment", payment);
         return "payments/getPayment";
     }
-
 
     @RequestMapping("/add")
     public String showFormToAddPayment(Model model) {
@@ -48,7 +67,6 @@ public class PaymentsController {
         return "redirect:/payments/all";
     }
 
-
     @RequestMapping("/edit/{id}")
     public String editPaymentForm(@PathVariable("id") Long id, Model model) {
         Payments payment = paymentsService.getPayment(id).get();
@@ -56,10 +74,10 @@ public class PaymentsController {
         return "payments/edit";
     }
 
-
     @PostMapping("/edit")
     public String editPayment(Payments payments) {
         paymentsService.editPayment(payments);
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+payments);
         return "redirect:/payments/all";
     }
 

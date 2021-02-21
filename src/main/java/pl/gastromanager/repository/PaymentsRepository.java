45df2847e6f7a9c0b6cc6698fb6e91bea3b9dcfe
Franciszek Szaniecki <franekszaniecki@gmail.com>
2021-02-21 22:02:27 +0100

    Payments views List

--- src/main/java/pl/gastromanager/repository/PaymentsRepository.java
+++ src/main/java/pl/gastromanager/repository/PaymentsRepository.java
@@ -1,9 +1,15 @@
 package pl.gastromanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.gastromanager.model.Payments;
 
+import java.util.List;
+
+
 @Repository
 public interface PaymentsRepository extends JpaRepository<Payments,Long> {
+    List<Payments> findAllByUsersId(long id);
+
 }
